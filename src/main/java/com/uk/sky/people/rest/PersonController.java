850f457172f2e9f7c594dea68174aa838abd7769
Bazsmith <bazsmith@me.com>
2023-02-08 14:38:32 +0000

    controller finished

--- src/main/java/com/uk/sky/people/rest/PersonController.java
+++ src/main/java/com/uk/sky/people/rest/PersonController.java
@@ -5,6 +5,7 @@ import com.uk.sky.people.domain.Person;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,6 +37,25 @@ public class PersonController {
     @GetMapping("/get/{id}")
     public Person getPerson(@PathVariable int id){ //pulls id from the path (url)
         return this.people.get(id);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public Person removePerson(@PathVariable int id){
+        return this.people.remove(id);
+
+    }
+
+    @PatchMapping("/update/{id}")
+    public Person updatePerson(@PathVariable int id, @PathParam("name") String name,@PathParam("age") Integer age,@PathParam("job") String job){
+        Person old = this.people.get(id);
+
+        if (name != null)old.setName(name);
+        if (age != null)old.setAge(age);
+        if (job != null)old.setJob(job);
+
+        return old;
+
+
     }
 
 
