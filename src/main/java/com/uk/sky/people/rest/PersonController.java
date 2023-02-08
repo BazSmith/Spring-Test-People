f89db23f21500d7cde0401a963e3d01e3b6279aa
Bazsmith <bazsmith@me.com>
2023-02-08 12:40:43 +0000

    get request by id

--- src/main/java/com/uk/sky/people/rest/PersonController.java
+++ src/main/java/com/uk/sky/people/rest/PersonController.java
@@ -1,6 +1,8 @@
 package com.uk.sky.people.rest;
 
+import com.uk.sky.people.PeopleApplication;
 import com.uk.sky.people.domain.Person;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -19,9 +21,21 @@ public class PersonController {
     }
 
     @PostMapping("/create")
-    public Person addPerson(@RequestBody Person person){
+    public Person addPerson(@RequestBody Person person){ //pulls person from the body of the request
         this.people.add(person); //add a new person to the list
         return this.people.get(this.people.size() - 1); //returns last person in the list
+    }
+
+
+    @GetMapping("/getall")
+    public List<Person> getAll(){
+        return this.people;
+    }
+
+
+    @GetMapping("/get/{id}")
+    public Person getPerson(@PathVariable int id){ //pulls id from the path (url)
+        return this.people.get(id);
     }
 
 
