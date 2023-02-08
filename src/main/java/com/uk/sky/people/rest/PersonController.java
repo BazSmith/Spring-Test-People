8bf4ff202fe049423df036083d461c14ed3d5f44
Bazsmith <bazsmith@me.com>
2023-02-08 11:52:59 +0000

    Post mapping added

--- src/main/java/com/uk/sky/people/rest/PersonController.java
+++ src/main/java/com/uk/sky/people/rest/PersonController.java
@@ -1,17 +1,30 @@
 package com.uk.sky.people.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.uk.sky.people.domain.Person;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PersonController {
+
+
+    private List<Person> people = new ArrayList<>();
 
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String greeting(){
         return "Hello Baz!";
     }
+
+    @PostMapping("/create")
+    public Person addPerson(@RequestBody Person person){
+        this.people.add(person); //add a new person to the list
+        return this.people.get(this.people.size() - 1); //returns last person in the list
+    }
+
+
 
 
 }
